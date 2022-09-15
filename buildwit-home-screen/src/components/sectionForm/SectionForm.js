8fb5cf68a4936c0be44fbffe034c35c5061f0d1c
faheem4531 <faheem4531@gmail.com>
2022-09-15 13:49:46 +0500

    Form Section Completed

--- buildwit-home-screen/src/components/sectionForm/SectionForm.js
+++ buildwit-home-screen/src/components/sectionForm/SectionForm.js
@@ -1,17 +1,34 @@
 import React from "react";
-import Form from './Form.css';
+import './Form.css';
+import '../section3/Section3.css';
 import Button from "../button/Button";
-import image from '../../images/sec-5-img.svg'
+import imageb from '../../images/sec-5-img.PNG';
+ 
 
 
-
-const SectionForm = () =>{
+const SectionForm = () => {
     return(
         <div className="section-form">
 
-          
+            <div className="section-form-card">
+              
+                    <h1 className="primary-heading">Inquire your Interest</h1>
+                    <div className="input-section top-space">
+                        <input className=" inputs margin-right" type="text" placeholder="Full Name" />
+                        <input className="inputs" type="text" placeholder="Username" />
+                    </div>
+                    <div className="input-section">
+                        <input className="inputs margin-right" type="email" placeholder="Email Address" />
+                        <input className="inputs" type="text" placeholder="Country" />
+                    </div>
+                    <textarea className="textarea"></textarea>
+                    <Button/>
+                
 
-        </div>
+            </div>  <img className="sec-5-img" src={imageb}/>
+             {/* <div className="image-div">
+               </div> */}
+            </div> 
     )
 }
 
